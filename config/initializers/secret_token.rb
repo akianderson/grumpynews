fe423e682e36bb1aad558b9a7f90d8350f8bfbe1
Aki Anderson <akianderson@Aki-Andersons-MacBook-Pro-2.local>
2013-09-21 12:37:37 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrumpyNews::Application.config.secret_key_base = '8cb5fc6e0cd784b3e7e75e6c06ec8a74f6fa5592b6db7ebac4e7901e3e2b2bdd7fdfcf83aa01bf4135764f5207f160a617a5f5c63122d76eedae1bb0a07dc55d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GrumpyNews::Application.config.session_store :cookie_store, key: '_grumpy_news_session'
