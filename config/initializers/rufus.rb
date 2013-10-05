d578125fae76e28bfb4e9ca6d84b7fd703162607
Aki Anderson <akianderson@Aki-Andersons-MacBook-Pro-2.local>
2013-10-04 22:03:38 -0500

    exception handling

--- config/initializers/rufus.rb
+++ config/initializers/rufus.rb
@@ -4,11 +4,15 @@ require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.start_new
 
 scheduler.every("15m") do
-	FeedEntry.delete_all
-	Feed.all.each do |feed|
-  	FeedEntry.update_from_feed(feed.feed_url)
-  	feed.last_retrieved = Time.now
-  	feed.save
-  	Rails.logger.info "i just saved a feed for #{feed}!!!"
-  end
+	begin
+		FeedEntry.delete_all
+		Feed.all.each do |feed|
+	  	FeedEntry.update_from_feed(feed.feed_url)
+	  	feed.last_retrieved = Time.now
+	  	feed.save
+	  	Rails.logger.info "i just saved a feed for #{feed}!!!"
+	  end
+	rescue
+		puts "oops db connection died"
+	end
 end
