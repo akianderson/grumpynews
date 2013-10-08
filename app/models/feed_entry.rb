1b45170182bd11afb164de64348093ecf5a8b848
Logan Buchanan <logan@fortyau.com>
2013-10-08 09:13:53 -0500

    feed changes

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -1,5 +1,11 @@
+require 'set'
+
 class FeedEntry < ActiveRecord::Base
-	def self.update_from_feed(feed_url)
+
+  GRUMPIES = Set.new ["war", "terror", "disease", "murder", "victim", "fear", "bomber", "suicide", "kill", "bomb", "kills", "victim", "victims", "sad", "nuclear", "chemical", "death"]
+
+
+  def self.update_from_feed(feed_url)
     feed = Feedzirra::Feed.fetch_and_parse(feed_url)
     add_entries(feed.entries)
   end
@@ -8,13 +14,15 @@ class FeedEntry < ActiveRecord::Base
   
   def self.add_entries(entries)
     entries.each do |entry|
+    classification = Set.new(entry.title.downcase.split(" ")).intersection(GRUMPIES).count > 0 ? 'G' : 'H'
       unless exists? :guid => entry.id
         create!(
-          :name         => entry.title,
-          :summary      => entry.summary,
-          :url          => entry.url,
-          :published_at => entry.published,
-          :guid         => entry.id
+          :name         	=> entry.title,
+          :summary      	=> entry.summary,
+          :url          	=> entry.url,
+          :published_at 	=> entry.published,
+          :guid         	=> entry.id,
+	  :classification 	=> classification
         )
       end
     end
