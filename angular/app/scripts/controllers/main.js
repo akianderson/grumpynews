1327d51cd6614231e6e4468b69bcbfe7df5edbe4
Aki Anderson <akianderson@Akis-MacBook-Pro.local>
2014-02-27 16:47:26 -0600

    add angular

---
+++ angular/app/scripts/controllers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('angularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeLinks = [
+      'Whole bunch of awesome links here',
+      'Text text text text text',
+      'Text so more link animations',
+      'So many words and phrases',
+      'Also, lots of kittens somewhere',
+      'More stuff to look square',
+      'More suqare looks better'
+    ];
+  });
